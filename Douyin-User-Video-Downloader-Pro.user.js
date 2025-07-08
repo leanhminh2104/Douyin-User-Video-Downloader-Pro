a06231ea6298dc2f42bfe1941aa8c5ee2f7be245
Lê Anh Minh <lamdev2104@gmail.com>
2025-07-08 11:05:42 +0700

    Update Douyin-User-Video-Downloader-Pro.user.js

--- Douyin-User-Video-Downloader-Pro.user.js
+++ Douyin-User-Video-Downloader-Pro.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @require      https://raw.githubusercontent.com/leanhminh2104/Speep-Wed-By-LAMDev/refs/heads/main/speep-wed-by-lamdev-v2.4.js
-// @name         Speep Wed By LAMDev
+// @require      https://raw.githubusercontent.com/leanhminh2104/Douyin-User-Video-Downloader-Pro/refs/heads/main/Douyin-User-Video-Downloader-Pro.js
+// @name         Douyin User Video Downloader Pro
 // @namespace    http://tampermonkey.net/
-// @version      2.4
-// @description  Menu quản lý Speep Wed By LAMDev
+// @version      4.0
+// @description  Douyin User Video Bulk Downloader Pro
 // @author       LAMDev
 // @match        *://*/*
 // @grant        none
-// @icon         https://i.pinimg.com/236x/26/fc/2f/26fc2faf0e4a6628341e1a6f975654e7.jpg
+// @icon         https://i.pinimg.com/236x/d9/04/b0/d904b0b6cbfbe4036fee930395671f4f.jpg
 // ==/UserScript==
